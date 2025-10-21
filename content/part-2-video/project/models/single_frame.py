b7cadd5ed47f906a05204fc85596bb3ece360c07
Tom <tb2r@icloud.com>
2025-10-21 16:29:27 +0200

    Single-frame model
    Single-frame model containing, model file, training and evaluation files. Also best and last checkpoints are added for model evaluation against test set

---
+++ content/part-2-video/project/models/single_frame.py
@@ -0,0 +1,74 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class SingleFrameCNN(nn.Module):
+    def __init__(self, num_classes):
+        super().__init__()
+        self.num_classes = num_classes
+        self.convolutional = nn.Sequential(
+
+                nn.Conv2d(3,16,3,padding=1), 
+                nn.BatchNorm2d(16),
+                nn.ReLU(),
+
+                nn.Conv2d(16,32,3,padding=1), #64->64
+                nn.BatchNorm2d(32),
+                nn.ReLU(),
+
+                nn.MaxPool2d(2,2), #64 - > 32 
+
+                nn.Conv2d(32,32,3,padding=1), #32 ->32
+                nn.BatchNorm2d(32),
+                nn.ReLU(),
+                
+                
+                nn.Conv2d(32,64,3,padding=1), #32 ->32
+                nn.BatchNorm2d(64),
+                nn.ReLU(),
+                
+                nn.MaxPool2d(2,2), #32 ->16
+
+                nn.Conv2d(64,128,3,padding=1), #16 ->16
+                nn.BatchNorm2d(128),
+                nn.ReLU(),
+                
+                
+                nn.Conv2d(128,128,3,padding=1), #16 ->16
+                nn.BatchNorm2d(128),
+                nn.ReLU(),
+                
+                nn.MaxPool2d(2,2), #16 -> 8
+
+
+                nn.Conv2d(128,128,3,padding=1), #8 ->8
+                nn.BatchNorm2d(128),
+                nn.ReLU(),
+                
+                
+                nn.Conv2d(128,128,3,padding=1), #8 ->8
+                nn.BatchNorm2d(128),
+                nn.ReLU(),
+            
+
+                nn.MaxPool2d(2,2), #8 -> 4
+                nn.Flatten(),
+                
+                nn.Linear(4*4*128, 128),
+                nn.BatchNorm1d(128),
+                nn.ReLU(),
+            
+                nn.Dropout1d(0.3),
+
+                nn.Linear(128, 64),
+                nn.BatchNorm1d(64),
+                nn.ReLU(),
+            
+                nn.Linear(64,num_classes)
+                )
+
+    def forward(self, x):
+            
+            return self.convolutional(x)
+
+  
