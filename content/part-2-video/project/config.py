ce1ec8ef126467cf72844c83fb1a37c74a7f637c
s214606 <s214606@student.dtu.dk>
2025-10-22 14:39:17 +0200

    Update experiment config  and api key

--- content/part-2-video/project/config.py
+++ content/part-2-video/project/config.py
@@ -1,22 +1,26 @@
 import logging
 import torch as t
-
+import wandb
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import Field
+
 
 logger = logging.getLogger(__name__)
 
 class Settings(BaseSettings):
-    root_dir: str = 'data/ufc10'
-
+    root_dir: str = '/dtu/datasets1/02516/ucf101_noleakage'#'data/ufc10'##
+    api_key: str = Field(..., alias='api_key')
     device: str = 'cuda' if t.cuda.is_available() else 'cpu'
 
     # seed: int = 42
     log_level: int = logging.INFO
 
-    model_config = SettingsConfigDict() # optional prefix for environment variables
+    model_config = SettingsConfigDict(env_file='.env') # optional prefix for environment variables
 
 settings = Settings()
 
 logger.info(f'Using device: {settings.device}')
+logger.info(f"Using dataset: {settings.root_dir}")
+logger.info(f"Logging on Weights & Biases")
 
-settings = Settings()
+wandb.login(key = settings.api_key)
---
+++ content/part-2-video/project/main.py
@@ -0,0 +1,106 @@
+from pipeline import Experiment
+from data.dataloaders import (
+    framevideostack_trainloader,
+    framevideostack_testloader,
+    frameimage_trainloader,
+    frameimage_testloader,
+)
+from models.early_fusion import EarlyFusion
+from models.late_fusion import LateFusion
+from models.single_frame import SingleFrameCNN
+from models.C3D import C3D
+import torch as t
+from config import settings
+
+loss_function = t.nn.CrossEntropyLoss()
+project_name = 'Video Classification'
+epochs = 50
+dataset = settings.root_dir.split('/')[-1]
+
+early_fusion = EarlyFusion(in_size=64)
+early_fusion_optimizer = t.optim.Adam(early_fusion.parameters(), lr=1e-4, weight_decay=1e-5)
+
+early_fusion_experiment = Experiment(
+    project_name=project_name,
+    name='Early Fusion',
+    config={
+        'train_loader': framevideostack_trainloader,
+        'test_loader': framevideostack_testloader,
+        'model': early_fusion,
+        'loss_function': loss_function,
+        'optimizer': early_fusion_optimizer,
+        'epochs': epochs,
+        'dataset': dataset
+    },
+    )
+
+
+late_fusion = LateFusion(
+    num_frames=10,
+    num_classes=10,
+    dropout_rate=0.5,
+    fusion='average_pooling'
+)
+
+late_fusion_optimizer = t.optim.Adam(late_fusion.parameters(), lr=1e-3, weight_decay=1e-5)
+
+late_fusion_experiment = Experiment(
+    project_name=project_name,
+    name='Late Fusion',
+    config={
+        'train_loader': framevideostack_trainloader,
+        'test_loader': framevideostack_testloader,
+        'model': late_fusion,
+        'loss_function': loss_function,
+        'optimizer': late_fusion_optimizer,
+        'epochs': epochs,
+        'dataset': dataset,
+    },
+    )
+
+
+single_frame = SingleFrameCNN(
+    num_classes=10,
+)
+single_frame_optimizer = t.optim.Adam(single_frame.parameters(), lr=1e-4, weight_decay=1e-5)
+
+single_frame_experiment = Experiment(
+    project_name=project_name,
+    name='Single-Frame CNN',
+    config={
+        'train_loader': frameimage_trainloader,
+        'test_loader': frameimage_testloader,
+        'model': single_frame,
+        'loss_function': loss_function,
+        'optimizer': single_frame_optimizer,
+        'epochs': epochs,
+        'dataset': dataset,
+    },
+    )
+
+c3d = C3D(
+    in_size=64,
+    num_classes=10,
+    num_frames=10
+)
+
+c3d_optimizer = t.optim.Adam(c3d.parameters(), lr=1e-4, weight_decay=1e-5)
+
+c3d_experiment = Experiment(
+    project_name=project_name,
+    name='3D CNN',
+    config={
+        'train_loader': framevideostack_trainloader,
+        'test_loader': framevideostack_testloader,
+        'model': c3d,
+        'loss_function': loss_function,
+        'optimizer': c3d_optimizer,
+        'epochs': epochs,
+        'dataset': dataset,
+    },
+    )
+
+single_frame_experiment.run()
+#early_fusion_experiment.run()
+#late_fusion_experiment.run()
+c3d_experiment.run()
